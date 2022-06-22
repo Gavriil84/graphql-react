b2ca631807ea3b25cdf55fc80be81184b3ae176b
Gavriil84 <gabwt.84@gmail.com>
2022-06-22 16:28:38 -0700

    Create people card component

---
+++ client/src/components/lists/People.js
@@ -0,0 +1,32 @@
+import { useQuery } from '@apollo/client'
+import { List } from 'antd'
+import React from 'react'
+import { GET_PEOPLE } from '../../queries'
+import Person from '../listItem/Person'
+
+const getStyles = () => ({
+    list: {
+        display: 'flex',
+        justifyContent: 'center'
+    }
+})
+
+const People = () => {
+    const styles = getStyles()
+
+    const { loading, error, data } = useQuery(GET_PEOPLE)
+    if (loading) return 'Loading...'
+    if (error) return `Error! ${error.message}`
+
+    return (
+        <List grid={{ gutter: 20, column: 1 }} style={styles.list}>
+            {data.people.map(({ id, firstName, lastName }) => (
+                <List.Item key={id}>
+                    <Person key={id} id={id} firstName={firstName} lastName={lastName} />
+                </List.Item>
+            ))}
+        </List>
+    )
+}
+
+export default People
