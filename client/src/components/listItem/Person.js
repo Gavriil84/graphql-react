d21bac88269b3beed09f420dd44a7f8356c22165
Gavriil84 <gabwt.84@gmail.com>
2022-06-26 21:57:32 -0700

    Add function to fetch car data

--- client/src/components/listItem/Person.js
+++ client/src/components/listItem/Person.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
+import { useQuery } from '@apollo/client'
 import { Card } from 'antd'
 import { EditOutlined } from '@ant-design/icons'
 import UpdatePerson from '../forms/UpdatePerson'
 import RemovePerson from '../buttons/RemovePerson'
-
+import { GET_CARS } from '../../queries'
+import Car from './Car'
 const getStyles = () => ({
     card: {
-        width: '500px'
+        width: '500px',
+        fontSize: '30px'
     }
 })
 
@@ -16,6 +19,11 @@ const Person = (props) => {
     const [firstName, setFirstName] = useState(props.firstName)
     const [lastName, setLastName] = useState(props.lastName)
     const [isEditing, setIsEditing] = useState(false)
+
+    const { loading, error, data } = useQuery(GET_CARS)
+
+    if (loading) return 'Loading...'
+    if (error) return `Error! ${error.message}`
 
     const handleButtonClick = () => {
         setIsEditing(!isEditing)
@@ -53,7 +61,11 @@ const Person = (props) => {
                             <RemovePerson id={id} />
                         ]}
                         style={styles.card}
-                    >{firstName} {lastName}
+                    >
+                        {firstName} {lastName}
+                        {data.cars.map(({ id, year, make, model, price, personId }) => (
+                            <Car type="inner" title={model} key={id} carId={id} id={props.id} year={year} make={make} model={model} price={price} personId={personId} />
+                        ))}
                     </Card>
                 )
             }
