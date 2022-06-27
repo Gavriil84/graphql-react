c8f2300aea1d5ac37cb35d94971366b8f62d3bbc
Gavriil84 <gabwt.84@gmail.com>
2022-06-26 17:14:58 -0700

    Small adjustment

--- client/src/components/listItem/Person.js
+++ client/src/components/listItem/Person.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { Card } from 'antd'
 import { EditOutlined } from '@ant-design/icons'
+import UpdatePerson from '../forms/UpdatePerson'
+import RemovePerson from '../buttons/RemovePerson'
 
 const getStyles = () => ({
     card: {
@@ -11,22 +13,51 @@ const getStyles = () => ({
 const Person = (props) => {
     const styles = getStyles()
     const [id] = useState(props.id)
-    const [firstName] = useState(props.firstName)
-    const [lastName] = useState(props.lastName)
+    const [firstName, setFirstName] = useState(props.firstName)
+    const [lastName, setLastName] = useState(props.lastName)
     const [isEditing, setIsEditing] = useState(false)
 
     const handleButtonClick = () => {
-        setIsEditing(true)
+        setIsEditing(!isEditing)
+    }
+
+    const updateStateVariable = (variables, value) => {
+        switch (variables) {
+            case 'firstName':
+                setFirstName(value)
+                break
+            case 'lastName':
+                setLastName(value)
+                break
+            default:
+                break
+        }
     }
 
     return (
-        <Card
-            actions={[
-                <EditOutlined key='edit' onClick={handleButtonClick} />
-            ]}
-            style={styles.card}
-        >{firstName} {lastName}
-        </Card>
+        <div>
+            {
+                isEditing ? (
+                    <UpdatePerson
+                        id={props.id}
+                        firstName={props.firstName}
+                        lastName={props.lastName}
+                        onButtonClick={handleButtonClick}
+                        updateStateVariable={updateStateVariable}
+                    />
+
+                ) : (
+                    <Card
+                        actions={[
+                            <EditOutlined key='edit' onClick={handleButtonClick} />,
+                            <RemovePerson id={id} />
+                        ]}
+                        style={styles.card}
+                    >{firstName} {lastName}
+                    </Card>
+                )
+            }
+        </div>
     )
 }
 
