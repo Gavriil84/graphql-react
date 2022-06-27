2f32e5222ea0e82c740b10d3371926362c261024
Gavriil84 <gabwt.84@gmail.com>
2022-06-26 19:42:40 -0700

    Add typeDef and resolvers for Cars

--- server/src/schema.js
+++ server/src/schema.js
@@ -9,15 +9,29 @@ const typeDefs = gql`
         lastName: String
     }
 
+    type Cars {
+        id: String!
+        year: Int
+        make: String
+        model: String
+        price: Float
+        personId: String
+    }
+
     type Query {
         person(id: String!): People
         people: [People]
+        car(id: String!): Cars
+        cars: [Cars]
     }
 
     type Mutation {
         addPerson(id: String, firstName: String!, lastName: String!): People
         updatePerson(id: String!, firstName: String, lastName: String): People
         deletePerson(id: String!): People
+        addCar(id: String, year: Int!, make: String!, model: String!, price: Float!, personId: String!): Cars
+        updateCar(id: String!, year: Int, make: String, model: String, price: Float, personId: String): Cars
+        deleteCar(id: String!): Cars
     }
 `
 
@@ -26,6 +40,10 @@ const resolvers = {
         people: () => people,
         person(parent, args, context, info) {
             return find(people, { id: args.id })
+        },
+        cars: () => cars,
+        car(parent, args, context, info) {
+            return find(cars, { id: args.id })
         }
     },
 
@@ -60,6 +78,43 @@ const resolvers = {
             remove(people, { id: args.id })
 
             return deletePerson;
+        },
+        addCar(root, args) {
+            const newCar = {
+                id: args.id,
+                year: args.year,
+                make: args.make,
+                model: args.model,
+                price: args.price,
+                personId: args.personId
+            }
+            cars.push(newCar)
+
+            return newCar
+        },
+        updateCar(root, args) {
+            const car = find(cars, { id: args.id })
+            if (!car) {
+                throw new Error('Car not found')
+            }
+
+            car.year = args.year
+            car.make = args.make
+            car.model = args.model
+            car.price = args.price
+            car.personId = args.personId
+
+            return car;
+        },
+        deleteCar(root, args) {
+            const deleteCar = find(cars, { id: args.id })
+            if (!deleteCar) {
+                throw new Error('Car not found')
+            }
+
+            remove(cars, { id: args.id })
+
+            return deleteCar
         }
 
     }
