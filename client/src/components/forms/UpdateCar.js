2ea470102c3abf7574d01c30611e32440dd182bc
Gavriil84 <gabwt.84@gmail.com>
2022-06-29 16:32:53 -0700

    Create update car component

---
+++ client/src/components/forms/UpdateCar.js
@@ -0,0 +1,118 @@
+import { useMutation } from '@apollo/client'
+import { Button, Form, Input } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { UPDATE_CAR } from '../../queries'
+
+const UpdateCar = props => {
+    const [id] = useState(props.id)
+    const [year, setYear] = useState(props.year)
+    const [make, setMake] = useState(props.make)
+    const [model, setModel] = useState(props.model)
+    const [price, setPrice] = useState(props.price)
+    const [personId, setPersonId] = useState(props.personId)
+    const [updateCar] = useMutation(UPDATE_CAR)
+
+    const [form] = Form.useForm()
+    const [, forceUpdate] = useState()
+
+    useEffect(() => {
+        forceUpdate({})
+    }, [])
+
+    const onFinish = values => {
+        const { year, make, model, price, personId } = values
+
+        updateCar({
+            variables: {
+                id,
+                year,
+                make,
+                model,
+                price,
+                personId
+            }
+        })
+
+        props.onButtonClick()
+    }
+
+    const updateStateVariable = (variable, value) => {
+        props.updateStateVariable(variable, value)
+        switch (variable) {
+            case 'year':
+                setYear(value)
+                break
+            case 'make':
+                setMake(value)
+                break
+            case 'model':
+                setModel(value)
+                break
+            case 'price':
+                setPrice(value)
+                break
+            case 'personId':
+                setPersonId(value)
+                break
+            default:
+                break
+        }
+    }
+
+    return (
+        <Form
+            form={form}
+            name='update-car-form'
+            layout='inline'
+            onfinish={onFinish}
+            initialValues={{
+                year: year,
+                make: make,
+                model: model,
+                price: price,
+                personId: personId
+            }}>
+            <Form.Item
+                name='year' rules={[{ required: true, message: 'Please enter a year' }]}>
+                <Input placeholder='2022'
+                    onChange={e => updateStateVariable('year', e.target.value)} />
+            </Form.Item>
+            <Form.Item
+                name='make' rules={[{ required: true, message: 'Please enter a maker' }]}>
+                <Input placeholder='Toyota'
+                    onChange={e => updateStateVariable('make', e.target.value)} />
+            </Form.Item>
+            <Form.Item
+                name='model' rules={[{ required: true, message: 'Please enter a model' }]}>
+                <Input placeholder='Prius'
+                    onChange={e => updateStateVariable('model', e.target.value)} />
+            </Form.Item>
+            <Form.Item
+                name='price' rules={[{ required: true, message: 'Please enter a price' }]}>
+                <Input placeholder='45000'
+                    onChange={e => updateStateVariable('price', e.target.value)} />
+            </Form.Item>
+            <Form.Item
+                name='personId' rules={[{ required: true, message: 'Please enter a personId' }]}>
+                <Input placeholder='12345'
+                    onChange={e => updateStateVariable('personId', e.target.value)} />
+            </Form.Item>
+            <Form.Item shouldUpdate={true}>
+                {() => (
+                    <Button
+                        type='primary'
+                        htmlType='submit'
+                        disabled={
+                            (!form.isFieldTouched('year') && !form.isFieldTouched('make')) && !form.isFieldTouched('model') && !form.isFieldTouched('price') && !form.isFieldTouched('personId') ||
+                            form.getFieldsError().filter(({ errors }) => errors.length).length
+                        }>
+                        Update Car
+                    </Button>
+                )}
+            </Form.Item>
+            <Button onClick={props.onButtonClick}>Cancel</Button>
+        </Form>
+    )
+}
+
+export default UpdateCar
