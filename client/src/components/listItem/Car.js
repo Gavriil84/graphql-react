970009853e05b864ab98f7cb66a77d1f24d3009b
Gavriil84 <gabwt.84@gmail.com>
2022-06-29 16:33:03 -0700

    Add props

--- client/src/components/listItem/Car.js
+++ client/src/components/listItem/Car.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Card } from 'antd'
 import { EditOutlined } from '@ant-design/icons'
 import RemoveCar from '../buttons/RemoveCar'
+import UpdateCar from '../forms/UpdateCar'
 
 const getStyles = () => ({
     card: {
@@ -11,7 +12,14 @@ const getStyles = () => ({
 })
 
 const Car = props => {
-    const { carId, id, year, make, model, price, personId, } = props
+    // const { carId, id, year, make, model, price, personId, } = props
+    const [year, setYear] = useState(props.year)
+    const [make, setMake] = useState(props.make)
+    const [model, setModel] = useState(props.model)
+    const [price, setPrice] = useState(props.price)
+    const [personId, setPersonId] = useState(props.personId)
+    const [carId, setCarId] = useState(props.carId)
+    const [id] = useState(props.id)
     const [isEditing, setIsEditing] = useState(false)
     const styles = getStyles()
 
@@ -19,25 +27,64 @@ const Car = props => {
         setIsEditing(!isEditing)
     }
 
+    const updateStateVariable = (variable, value) => {
+        switch (variable) {
+            case 'year':
+                setYear(value)
+                break
+            case 'make':
+                setMake(value)
+                break
+            case 'model':
+                setModel(value)
+                break
+            case 'price':
+                setPrice(value)
+                break
+            case 'personId':
+                setPersonId(value)
+                break
+            default:
+                break
+        }
+    }
+
     return (
         <div>
-            {id == personId ? (
-                <Card
-                    type="inner"
-                    actions={[
-                        <EditOutlined key="edit" onClick={handleButtonClick} />,
-                        <RemoveCar id={carId} />
-                    ]}
-                    title={model}
-                    style={styles.card}
-                >
-                    Year: {year}<br />
-                    Make: {make}<br />
-                    Price: ${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                </Card>
-            ) : (
-                null
-            )}
+            {
+                isEditing ? (
+                    <UpdateCar
+                        id={props.id}
+                        year={props.year}
+                        make={props.make}
+                        model={props.model}
+                        price={props.price}
+                        personId={props.personId}
+                        onButtonClick={handleButtonClick}
+                        updateStateVariable={updateStateVariable} />
+                ) : (
+                    <div>
+                        {id == personId ? (
+                            <Card
+                                type="inner"
+                                actions={[
+                                    <EditOutlined key="edit" onClick={handleButtonClick} />,
+                                    <RemoveCar id={carId} />
+                                ]}
+                                title={model}
+                                style={styles.card}
+                            >
+                                Year: {year}<br />
+                                Make: {make}<br />
+                                Price: ${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                            </Card>
+                        ) : (
+                            null
+                        )}
+                    </div>
+                )
+            }
+
         </div>
 
     )
